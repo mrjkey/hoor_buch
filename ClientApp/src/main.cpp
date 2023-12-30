5adfb721ae7e33edd4369fc5448f226352afc257
Jared Key <key.jared.a@gmail.com>
2023-12-30 16:34:17 +0100

    building a testing

--- ClientApp/src/main.cpp
+++ ClientApp/src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "math_utils.h"
 
 int main(int argc, char **argv)
 {
+    std::cout << "1 + 2 = " << add(1, 2) << std::endl;
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
---
+++ ClientApp/src/math_utils.cpp
@@ -0,0 +1,7 @@
+// ClientApp/src/math_utils.cpp
+#include "math_utils.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
