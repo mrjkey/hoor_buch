ea021e43bf9cc220e4e68995f9888804862ee7c7
Jared Key <key.jared.a@gmail.com>
2024-01-04 14:42:39 +0100

    ipv6

--- ClientApp/src/connection.cpp
+++ ClientApp/src/connection.cpp
@@ -6,14 +6,18 @@ namespace asio = boost::asio;
 
 void start_server(asio::io_context &io_context, short port)
 {
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
-    std::cout << "Server listening on port " << port << std::endl;
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v6(), port)); // Use IPv6
+    std::cout << "Server listening on port " << port << " (IPv6)" << std::endl;
+
+    // tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
+    // std::cout << "Server listening on port " << port << std::endl;
 
     while (true)
     {
         tcp::socket socket(io_context);
         acceptor.accept(socket);
         std::string message = "Hello from Server!";
+        std::cout << "Server sending: " << message << std::endl;
         boost::system::error_code ignored_error;
         boost::asio::write(socket, asio::buffer(message), ignored_error);
     }
@@ -22,7 +26,10 @@ void start_server(asio::io_context &io_context, short port)
 void start_client(asio::io_context &io_context, const std::string &server, short port)
 {
     tcp::socket socket(io_context);
-    socket.connect(tcp::endpoint(asio::ip::address::from_string(server), port));
+    socket.connect(tcp::endpoint(asio::ip::make_address_v6(server), port)); // Use IPv6
+
+    // tcp::socket socket(io_context);
+    // socket.connect(tcp::endpoint(asio::ip::address::from_string(server), port));
 
     std::vector<char> buffer(128);
     boost::system::error_code error;
